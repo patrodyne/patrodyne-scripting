758a41a5f10f8d8dfb34d22a63fcd2ceb9e5453b
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2012-07-24 19:35:25 -0400

    Conditionally switch to runFromPOM.sh.

--- Java/runFromJAR.sh
+++ Java/runFromJAR.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-java \
-	-Dorg.patrodyne.scripting.java.sourcepath="demo" \
-	-Dorg.patrodyne.scripting.java.classpath="demo" \
-	-jar target/patrodyne-scripting-java-1.0.0-SNAPSHOT.jar \
-	demo/HelloWorld.java "arg0" "arg1" "arg2"
+if [ -r target ]; then
+	java \
+		-Dorg.patrodyne.scripting.java.sourcepath="demo" \
+		-Dorg.patrodyne.scripting.java.classpath="demo" \
+		-jar target/patrodyne-scripting-java-*.jar \
+		demo/HelloWorld.java "arg0" "arg1" "arg2"
+else
+	./runFromPOM.sh
+fi
 # vi:set tabstop=4 hardtabs=4 shiftwidth=4:
